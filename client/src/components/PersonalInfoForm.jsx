4f4e98e98ffed9db63356be476797e7394c0771a
vivek210404 <vivek210404@gmail.com>
2026-02-02 00:15:26 +0530

    Fixed image issue

--- client/src/components/PersonalInfoForm.jsx
+++ client/src/components/PersonalInfoForm.jsx
@@ -85,7 +85,7 @@ const PersonalInfoForm = ({
                   : URL.createObjectURL(data.image)
               }
               alt="user-image"
-              className="w-16 h-16 rounded-full object-cover mt-5 ring ring-slate-300 hover:opacity-80"
+              className="w-16 h-16 rounded-full object-cover mt-5 ring ring-slate-300 hover:opacity-80 bg-white"
             />
           ) : (
             <div className="inline-flex items-center gap-2 mt-5 text-slate-600 hover:text-slate-700 cursor-pointer">
--- client/src/components/templates/MinimalImageTemplate.jsx
+++ client/src/components/templates/MinimalImageTemplate.jsx
@@ -18,12 +18,12 @@ const MinimalImageTemplate = ({ data, accentColor }) => {
                     {/* Image */}
                     {data.personal_info?.image && typeof data.personal_info.image === 'string' ? (
                         <div className="mb-6">
-                            <img src={data.personal_info.image} alt="Profile" className="w-32 h-32 object-cover rounded-full mx-auto" style={{ background: accentColor+'70' }} />
+                            <img src={data.personal_info.image} alt="Profile" className="w-32 h-32 object-cover rounded-full mx-auto" style={{ background: accentColor?.startsWith('#') ? accentColor + '70' : '#' + (accentColor || '3b82f6') + '70' }} />
                         </div>
                     ) : (
                         data.personal_info?.image && typeof data.personal_info.image === 'object' ? (
                             <div className="mb-6">
-                                <img src={URL.createObjectURL(data.personal_info.image)} alt="Profile" className="w-32 h-32 object-cover rounded-full mx-auto" />
+                                <img src={URL.createObjectURL(data.personal_info.image)} alt="Profile" className="w-32 h-32 object-cover rounded-full mx-auto" style={{ background: accentColor?.startsWith('#') ? accentColor + '70' : '#' + (accentColor || '3b82f6') + '70' }} />
                             </div>
                         ) : null
                     )}
